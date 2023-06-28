c3eed6e9cf743e59418da66bb393c69393f57dc6
Elie Andraos <elieandraos31@gmail.com>
2023-06-29 00:34:12 +0300

    :bulb: change page title and add comments

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-2/3 h-3/4">
-        <div class="p-4 shadow-lg text-sm bg-gray-800 rounded-lg leading-normal overflow-scroll h-full">
-
+        <div class="p-4 shadow-lg bg-gray-800 rounded-lg leading-normal overflow-scroll h-full">
+            <!-- terminal header bar -->
             <div class="flex items-center">
                 <div class="h-3 w-3 bg-red-500 rounded-full"></div>
                 <div class="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
@@ -9,6 +9,7 @@
             </div>
 
             <div class="space-y-4 my-6">
+                <!-- Commands history and preview -->
                 <div v-for="(command,index) in commandsHistory" :key="index">
                     <div class="flex">
                         <span class="text-sky-300">devs</span>
@@ -18,6 +19,7 @@
                     <component :is="previewCommand(command)" class="my-4"/>
                 </div>
 
+                <!-- Command input -->
                 <div class="flex">
                     <span class="text-sky-300">devs</span>
                     <span class="text-yellow-100">@primitive:~$</span>
@@ -54,7 +56,7 @@ const parseCommand = () => {
 
 const previewCommand = command => {
     const foundCommand = availableCommands.find(item => item.command === command)
-    return foundCommand ? foundCommand.component : null //@todo: return commmandNotFound component
+    return foundCommand ? foundCommand.component : null
 }
 
 onMounted( () => cliInput.value.focus() )
