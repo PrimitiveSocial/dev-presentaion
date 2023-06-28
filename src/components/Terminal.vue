3fef31b8f184888fa8727ecee81c74e065174863
Elie Andraos <elieandraos31@gmail.com>
2023-06-28 23:57:12 +0300

    :closed_lock_with_key: save command history

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -1,22 +1,48 @@
 <template>
     <div class="w-2/3 h-3/4">
         <div class="p-4 shadow-lg text-sm bg-gray-800 rounded-lg leading-normal overflow-scroll h-full">
+
             <div class="flex items-center">
                 <div class="h-3 w-3 bg-red-500 rounded-full"></div>
                 <div class="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
                 <div class="ml-2 h-3 w-3 bg-green-500 rounded-full"></div>
             </div>
-            <div class="my-6 flex">
-                <span class="text-sky-300">devs</span>
-                <span class="text-yellow-100">@primitive:~$</span>
-                <input
-                    class="ml-2 text-gray-500 placeholder-gray-500 bg-transparent border-none w-full focus:ring-0 focus:outline-none"
-                    placeholder="confused? type help and hit enter to get started."
-                />
+
+            <div class="space-y-4 my-6">
+                <div v-for="(command,index) in commandsHistory" :key="index" class="flex">
+                    <span class="text-sky-300">devs</span>
+                    <span class="text-yellow-100">@primitive:~$</span>
+                    <span class="ml-2 text-green-300">{{ command }}</span>
+                </div>
+
+                <div class="flex">
+                    <span class="text-sky-300">devs</span>
+                    <span class="text-yellow-100">@primitive:~$</span>
+                    <input
+                        ref="cliInput"
+                        v-model="command"
+                        class="ml-2 text-green-500 placeholder-gray-500 bg-transparent border-none w-full focus:ring-0 focus:outline-none"
+                        placeholder="type help and hit enter to get started."
+                        @keyup.enter="parseCommand"
+                    />
+                </div>
             </div>
+
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+
+const command = ref()
+const cliInput = ref()
+const commandsHistory = ref([])
+
+const parseCommand = () => {
+    commandsHistory.value.push(command.value)
+    command.value = ''
+}
+
+onMounted( () => cliInput.value.focus() )
 </script>
