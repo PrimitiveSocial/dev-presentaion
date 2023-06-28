315c3d1c3acdda73e656a3611a68236f749f0535
Elie Andraos <elieandraos31@gmail.com>
2023-06-29 00:18:39 +0300

    :safety_vest: parse command and preview component

---
+++ src/components/HelpCommand.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>this is the help command preview</p>
+</template>
--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -9,10 +9,13 @@
             </div>
 
             <div class="space-y-4 my-6">
-                <div v-for="(command,index) in commandsHistory" :key="index" class="flex">
-                    <span class="text-sky-300">devs</span>
-                    <span class="text-yellow-100">@primitive:~$</span>
-                    <span class="ml-2 text-green-300">{{ command }}</span>
+                <div v-for="(command,index) in commandsHistory" :key="index">
+                    <div class="flex">
+                        <span class="text-sky-300">devs</span>
+                        <span class="text-yellow-100">@primitive:~$</span>
+                        <span class="ml-2 text-green-300">{{ command }}</span>
+                    </div>
+                    <component :is="previewCommand(command)" class="my-4"/>
                 </div>
 
                 <div class="flex">
@@ -34,14 +37,24 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import HelpCommand from './HelpCommand.vue'
 
-const command = ref()
+const command = ref('')
 const cliInput = ref()
 const commandsHistory = ref([])
+
+const availableCommands = [
+    { command: 'help', component: HelpCommand }
+]
 
 const parseCommand = () => {
     commandsHistory.value.push(command.value)
     command.value = ''
+}
+
+const previewCommand = command => {
+    const foundCommand = availableCommands.find(item => item.command === command)
+    return foundCommand ? foundCommand.component : null //@todo: return commmandNotFound component
 }
 
 onMounted( () => cliInput.value.focus() )
