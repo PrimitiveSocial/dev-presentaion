082feb4f3f50f8a4c78b2c879b8844c4c8f69dcf
Elie Andraos <elieandraos31@gmail.com>
2023-07-07 15:49:55 +0300

    :alembic: test prod build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    transforms: [
+      {
+        test: /\.vue$/,
+        transform: (src) => {
+          return {
+            code: src,
+            map: null,
+            headers: { 'Content-Type': 'application/javascript' },
+          }
+        },
+      },
+    ],
+  },
 })
