3b9ae21a4dc5715659c2f8367717944bf174126f
matthewPrimitive <matthew@primitivesocial.com>
2023-07-10 09:28:50 -0500

    added more trivia

--- src/components/TriviaQuestions.js
+++ src/components/TriviaQuestions.js
@@ -1,7 +1,7 @@
 export const questions = [
     {
         'question': 'What will counter be equal to?',
-        'answer': '5',
+        'answer': '10',
         'options': [
             '10',
             '4',
@@ -28,6 +28,34 @@ export const questions = [
         ],
         'module': {
             'value': 'You have ran into unforeseen blockers when building out a webpage, and now the estimate is way off.',
+            'isCode': false
+        }
+    },
+    {
+        'question': 'What does WYSIWYG stand for',
+        'answer': 'What You See Is What You Get',
+        'options': [
+            'Whimsical Yachts Sailing in Waves, Yearning for Glory',
+            'What You See Is What You Get',
+            'Wondrously Yellow Sunsets Illuminate Whimsical Yearnings of Grace',
+            'Whimsical Yetis Seek Ice-Cream With Yearning Gazes'
+        ],
+        'module': {
+            'value': 'W... Y... S... I... W... Y... G...',
+            'isCode': false
+        }
+    },
+    {
+        'question': 'Who loves Tailwind the most?',
+        'answer': 'Josh',
+        'options': [
+            'Elie',
+            'Michael',
+            'Matthew',
+            'Josh'
+        ],
+        'module': {
+            'value': 'Tailwindcss',
             'isCode': false
         }
     },
