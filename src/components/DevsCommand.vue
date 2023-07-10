6a0092ea1a02f874108a279adf01b3a026b8e6d5
matthewPrimitive <matthew@primitivesocial.com>
2023-07-10 09:46:56 -0500

    :sparkles: Added new image of jbird :bird:

--- src/components/DevsCommand.vue
+++ src/components/DevsCommand.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="my-4 px-6 flex justify-between gap-4">
       <div class="w-1/2">
-        <img class="aspect-video object-cover" src="/jerred.jpg" alt="">
+        <img class="aspect-video object-cover" src="/jerred-party.jpg" alt="">
       </div>
       <div class="w-1/2">
         <img class="aspect-video object-cover" src="/fish-josh.jpg" alt="">
