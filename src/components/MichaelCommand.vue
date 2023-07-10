8f3fc240036fb76aa95e80ddf6b4ad4a70655156
Michael Goldberg <michael@leadwithprimitive.com>
2023-07-10 10:15:37 -0400

    adjusted michael and workflow

--- src/components/MichaelCommand.vue
+++ src/components/MichaelCommand.vue
@@ -8,7 +8,7 @@
                   <li class="leading-6 pb-3">From Livingston, NJ</li>
                   <li class="leading-6-none pb-3">Studied graphic design at Montclair State University, NJ. Graduated with a Bachelor of Arts.</li>
                   <li class="leading-6 pb-3">Worked as a Graphic & UI/UX designer for 8 years. </li>
-                  <li class="leading-6 pb-3">Became bored of the tediousness of design but wanted to use my skills and knowledge in a similar field. Became a front-end developer at Primitive in September of 2019. </li>
+                  <li class="leading-6 pb-3">Became bored and burnt out of the tediousness of design but wanted to use my skills and knowledge in a similar field. I enjoyed the problem solving aspect of design, and realized that development uses a lot of problem solving so I became a front-end developer at Primitive in September of 2019. </li>
                 </ul>
             </div>
         </div>
@@ -17,7 +17,7 @@
                 My primitive journey
             </h3>
             <ul class="leading-8 list-disc ml-6 leading-6">
-                <li class="leading-6 pb-3"> Started in 2019 </li>
+                <li class="leading-6 pb-3"> Started at Primitive in the Fall of 2019. First met with Trey, in my car in torrential downpour rain, on Zoom. </li>
                 <li class="leading-6 pb-3"> Work on only front-end development </li>
                 <li class="leading-6 pb-3"><strong> Strengths:</strong> Adaptability, Input, Includer, Arranger, Positivity </li>
             </ul>
--- src/components/WorkflowCommand.vue
+++ src/components/WorkflowCommand.vue
@@ -5,7 +5,7 @@
                 Front-end
             </h3>
             <div class="space-y-2 leading-6">
-                <p> It's the visual side of development. What a designer creates, we convert into a functioning, visible website. </p>
+                <p> It's the visual side of development. Whatever the designer creates, we convert into a visible, functioning website. </p>
                 <strong class="py-2 block relative w-full">
                     <span class="font-bold text-purple-500">W</span>hat
                     <span class="font-bold text-purple-500">Y</span>ou
@@ -15,16 +15,35 @@
                     <span class="font-bold text-purple-500">Y</span>ou
                     <span class="font-bold text-purple-500">G</span>et
                 </strong>
-              <p> The code we write can be viewed and inspected in the web browser, phone or tablet device. </p>
+              <strong class="block relative pb-3 font-bold text-purple-500"> WYSIWYG </strong>
+              <p> The code we write can be viewed and inspected in the web browser, phone or tablet device. It's almost like design, but with computer code. We control the size / width of elements, where they are positioned on the page, colors, font sizes, etc. </p>
             </div>
 
             <ul class="leading-6 text-sm mt-8 bg-slate-700 p-4 bg-opacity-60 rounded-lg space-y-2 list-disc pl-8">
                 <li>Front-end developers primarily use HTML, CSS, PHP & Javascript development languages</li>
-                <li>  We use frameworks or libraries that are built off of the above languages. VUE, React, Tailwind (uhum Josh, Jerred's favorite - thank you Jerred!)</li>
+                <li>  We also use frameworks or libraries that are built off of the above languages. VUE, React, Tailwind (uhum Josh, Jerred's favorite - thank you Jerred!) Frameworks make building things faster, easier.
+                  <br>
+                  <em class="py-2 block">Example: Slick Slider for sliders. </em>
+                </li>
+
                 <li> The browser's console / inspector is his best friend. This tool allows us to monitor front end code in real time. We can also make adjustments on the fly and they are visible right away. </li>
-                <li>Implement assets: images, videos & fonts</li>
+
+              <li>Implement assets: images, videos & fonts </li>
+
                 <li>Build using responsive support / multi-device support. It’s 2023 and you can expect a website visitor to be using various devices. </li>
+
             </ul>
+
+          <ul class="leading-6 text-sm mt-8 bg-slate-700 p-4 bg-opacity-60 rounded-lg space-y-2 list-disc pl-8">
+            <strong>From Start To Finish</strong>
+            <li>  1. Get assigned project via Clickup and take a look at the assets provided to us. This is a time to do time estimates, ask for assets or ask questions.</li>
+            <li> 2. Gather all assets. Images, fonts, content, design.</li>
+            <li> 3. Start building the project in our code editor. We also keep in mind where the final site will be viewed. Bind, WordPress, HubSpot. </li>
+            <li> 4. Finalize the build, add a userback snippet and submit to internal review. </li>
+            <li> 5. Take care of edits that come through.
+            <li> 6. Once all of the above is completed, we can deploy the site live.</li>
+          </ul>
+
         </div>
         <div class="w-1/2 pl-5">
             <h3 class="capitalize mb-6 bg-gradient-to-r from-purple-600 to-gray-800 bg-opacity-20 px-4 py-3 rounded-lg">
